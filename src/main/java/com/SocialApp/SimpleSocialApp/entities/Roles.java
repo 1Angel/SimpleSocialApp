c479f19604327abdf00a01b1569145c2bcf83644
Angel Junior Abreu <iangeeel01@gmail.com>
2024-04-27 20:36:24 -0400

    somes changees

--- src/main/java/com/SocialApp/SimpleSocialApp/entities/Roles.java
+++ src/main/java/com/SocialApp/SimpleSocialApp/entities/Roles.java
@@ -1,63 +1,7 @@
 package com.SocialApp.SimpleSocialApp.entities;
 
-import java.util.Date;
-
-import org.hibernate.annotations.CreationTimestamp;
-import java.util.List;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-
-@Entity
-public class Roles {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
-
-    @Column(unique = true)
-    private String name;
-
-    @CreationTimestamp
-    private Date createdDate;
-
-    public Roles() {
-    }
-
-    public Roles(Long id, String name, Date createdDate) {
-        Id = id;
-        this.name = name;
-        this.createdDate = createdDate;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-
+public enum Roles {
+    Admin,
+    user,
 
 }
